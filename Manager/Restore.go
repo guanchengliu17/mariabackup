423260dcdc4e2b32ab900cb0ab16dd612ba04b7c
Sergei Jeldoshev <sergei.jeldoshev@scoro.com>
2020-01-21 17:01:06 +0200

    Added RestoreManager decompressBackup function

--- Manager/Restore.go
+++ Manager/Restore.go
@@ -1,1 +1,122 @@
 package Manager
+
+import (
+        "compress/gzip"
+        "errors"
+        "fmt"
+        "io"
+        "os"
+        "os/exec"
+        "path/filepath"
+	"strconv"
+	"io/ioutil"
+	"log"
+)
+
+type RestoreManager struct {
+        sourceDirectory string
+        targetDirectory string
+}
+
+func CreateRestoreManager(
+        SourceDirectory string,
+        TargetDirectory string) (*RestoreManager, error) {
+
+        return &RestoreManager{
+                sourceDirectory: SourceDirectory,
+                targetDirectory: TargetDirectory,
+        }, nil
+
+}
+
+func (b *RestoreManager) Restore() error {
+	log.SetFlags(log.Ldate | log.Ltime)
+
+	err := os.RemoveAll(filepath.Join(b.sourceDirectory, "restore"))
+	if err != nil {
+		return errors.New(fmt.Sprintf("[Restore backup]> Failed to remove previous backup restore directory, %v", err))
+        }
+
+	backupSubDirectory := ""
+
+	backupPosition, _ := b.getBackupPosition()
+
+	for i := 0;  i <= backupPosition; i++ {
+		if i == 0 {
+			backupSubDirectory = "full"
+		} else {
+			backupSubDirectory = filepath.Join("incr", strconv.Itoa(i))
+		}
+
+		log.Println("Decompressing", filepath.Join(filepath.Join(b.sourceDirectory, backupSubDirectory), "backup.gz"), "to", filepath.Join(b.sourceDirectory, "restore", backupSubDirectory))
+		err := b.decompressBackup(backupSubDirectory)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (b *RestoreManager) decompressBackup(backupSubDirectory string) error {
+	backupRestoreTemporaryDirectory := filepath.Join(b.sourceDirectory, "restore", backupSubDirectory)
+
+	err := os.MkdirAll(backupRestoreTemporaryDirectory, 750)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("[RestoreManager]> Making directories failed, %v", err))
+	}
+
+	f, err := os.Open(filepath.Join(filepath.Join(b.sourceDirectory, backupSubDirectory), "backup.gz"))
+
+        if err != nil {
+                return err
+        }
+
+        defer f.Close()
+
+        gzr, err := gzip.NewReader(f)
+
+        if err != nil {
+                return err
+        }
+
+        defer gzr.Close()
+
+        command := exec.Command("mbstream", "-x", "-C", backupRestoreTemporaryDirectory)
+
+        out, err := command.StdinPipe()
+        command.Stderr = os.Stderr
+        if err != nil {
+                return err
+        }
+
+        err = command.Start()
+
+        if err != nil {
+                return errors.New(fmt.Sprintf("[RestoreManager Restore()]> Failed executing command: %v", err))
+        }
+
+        _, err = io.Copy(out, gzr)
+
+        if err != nil {
+                return err
+        }
+
+	return nil
+}
+
+func (b *RestoreManager) getBackupPosition() (int, error) {
+	data, err := ioutil.ReadFile(filepath.Join(b.sourceDirectory, "mariabackup.pos"))
+        if err != nil {
+		return 0, errors.New(fmt.Sprintf("[RestoreManager]> Failed to read backup position file, %v", err))
+	}
+
+	backupPosition, err := strconv.Atoi(string(data))
+
+	if err != nil {
+                return 0, err
+        }
+
+	return backupPosition, nil
+}
--- main.go
+++ main.go
@@ -10,17 +10,18 @@ import (
 
 //backup command
 var BackupCommand = flag.NewFlagSet("backup", flag.ExitOnError)
-var Host = BackupCommand.String("host", "localhost", "database host")
-var Port = BackupCommand.Int("port", 3306, "database port")
-var Username = BackupCommand.String("username", "mariabackup", "database username")
-var Password = BackupCommand.String("password", "", "database password")
-var Type = BackupCommand.String("type", "full", "backup type - full|incremental")
-var TargetDir = BackupCommand.String("target-dir", "/backup/mariabackup", "directory in which the backups will be placed")
-var DataDir = BackupCommand.String("datadir", "/var/lib/mysql", "directory where the MySQL data is stored")
+var BackupHost = BackupCommand.String("host", "localhost", "database host")
+var BackupPort = BackupCommand.Int("port", 3306, "database port")
+var BackupUsername = BackupCommand.String("username", "mariabackup", "database username")
+var BackupPassword = BackupCommand.String("password", "", "database password")
+var BackupType = BackupCommand.String("type", "full", "backup type - full|incremental")
+var BackupTargetDir = BackupCommand.String("target-dir", "/backup/mariabackup", "directory in which the backups will be placed")
+var BackupDataDir = BackupCommand.String("datadir", "/var/lib/mysql", "directory where the MySQL data is stored")
 
 //restore command
 var RestoreCommand = flag.NewFlagSet("restore", flag.ExitOnError)
-var SourceDir = RestoreCommand.String("source-dir", "/backup/mariabackup", "directory in which the backups are stored")
+var RestoreSourceDir = RestoreCommand.String("source-dir", "/backup/mariabackup", "directory in which the backups are stored")
+var RestoreTargetDir = RestoreCommand.String("target-dir", "/var/lib/mysql", "directory where the MySQL data is stored")
 
 func main() {
 
@@ -41,17 +42,17 @@ func main() {
 		}
 		//do backup
 
-		log.Println("Backup data directory:", *DataDir)
-		log.Println("Backup target directory:", *TargetDir)
+		log.Println("Backup data directory:", *BackupDataDir)
+		log.Println("Backup target directory:", *BackupTargetDir)
 
 		backup, err := Manager.CreateBackupManager(
-			*TargetDir,
-			*Host,
-			*Port,
-			*Username,
-			*Password,
-			*Type,
-			*DataDir,
+			*BackupTargetDir,
+			*BackupHost,
+			*BackupPort,
+			*BackupUsername,
+			*BackupPassword,
+			*BackupType,
+			*BackupDataDir,
 		)
 
 		if err != nil {
@@ -76,8 +77,25 @@ func main() {
 		}
 		//do restore
 
-		log.Println("Todo: needs implementation..")
-		log.Println("Source dir", *SourceDir)
+		log.Println("Restore source directory:", *RestoreSourceDir)
+		log.Println("Restore target directory:", *RestoreTargetDir)
+
+		restore, err := Manager.CreateRestoreManager(*RestoreSourceDir, *RestoreTargetDir)
+
+		if err != nil {
+                        log.Printf("Failed to initialize restore")
+                        return
+                }
+
+                err = restore.Restore()
+
+                if err != nil {
+                        log.Println("Restore has failed:", err)
+                        return
+                }
+
+                log.Printf("Restore successfully finished")
+
 	default:
 		fmt.Printf("%q is not valid command\n", os.Args[1])
 		return
