ad652b18c0763bcc98c5657e8064f035e115f92e
Karl Markus Jõgila <karl.jogila@scoro.com>
2021-08-30 15:57:49 +0300

    Change exec.sh

--- exec.sh
+++ exec.sh
@@ -2,5 +2,3 @@
 
 sudo mariabackup --host=127.0.0.1 --port=3306 --user=root --password= --backup --version-check --datadir=/var/lib/mysql --target_dir=/backup/mariabackup_manual/ --extra-lsndir=/backup/mariabackup_manual/ --parallel=6 --stream=xbstream  > backup.xb
 #sudo mariabackup --host=127.0.0.1 --port=3306 --user=root --password= --backup --version-check --datadir=/var/lib/mysql --target_dir=/backup/mariabackup_manual/ --extra-lsndir=/backup/mariabackup_manual/ --parallel=6 --stream=xbstream  | pigz -p 12 --fast > backup.x
-
-git remote add github git@github.com:karlmjogila/mariabackup.git
